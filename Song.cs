4fd7aaf334d08746dc1c50ae3282871c40a1fc37
Melissa Gwyn <melissagwyn@gmail.com>
2023-04-11 15:04:40 -0400

    Created 3 new classes

---
+++ Song.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RhythmsGonnaGetYou
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public int TrackNumber { get; set; }
+        public string Title { get; set; }
+        public DateTime Duration { get; set; }
+        public int AlbumId { get; set; }
+    }
+}
